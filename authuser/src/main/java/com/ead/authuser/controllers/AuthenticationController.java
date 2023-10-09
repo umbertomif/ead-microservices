7a1a435f740d52c387ed9028ba2f0a917ec3bcb3
Umberto M. I. Ferreira <umberto.mif@gmail.com>
2023-10-09 16:17:32 +0900

    Added validations.

--- authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
+++ authuser/src/main/java/com/ead/authuser/controllers/AuthenticationController.java
@@ -10,11 +10,11 @@ import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
-import java.util.UUID;
 
 @RestController
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -26,7 +26,7 @@ public class AuthenticationController {
 
     @PostMapping("/signup")
     public ResponseEntity<Object> registerUser(
-            @RequestBody
+            @RequestBody @Validated(UserDto.UserView.RegistrationPost.class)
             @JsonView(UserDto.UserView.RegistrationPost.class) UserDto userDto
     ) {
         if (userService.existsByUsername(userDto.getUserName())) {

6293fe89015d3e0173e8367bc266845ad7005f0f
Umberto M. I. Ferreira <umberto.mif@gmail.com>
2023-11-03 12:41:06 +0900

    Added the relationship between classes.

--- course/src/main/java/com/ead/course/models/ModuleModel.java
+++ course/src/main/java/com/ead/course/models/ModuleModel.java
@@ -2,12 +2,16 @@ package com.ead.course.models;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 
 import javax.persistence.*;
 import java.io.Serializable;
 import java.time.LocalDateTime;
 import java.util.UUID;
+import java.util.Set;
 
 @Data
 @JsonInclude(JsonInclude.Include.NON_NULL) // hide all the nullable attributes from return
@@ -29,4 +33,13 @@ public class ModuleModel implements Serializable {
     @Column(nullable = false)
     @JsonFormat(shape = JsonFormat.Shape.STRING,pattern = "dd-MM-yyyy HH:mm:ss")
     private LocalDateTime creationDate;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private CourseModel course;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @OneToMany(mappedBy = "module", fetch = FetchType.LAZY)
+    @Fetch(FetchMode.SUBSELECT)
+    private Set<LessonModel> lessons;
 }

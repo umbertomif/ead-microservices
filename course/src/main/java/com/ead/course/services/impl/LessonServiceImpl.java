f887366be548c4a9d7d3a995ee5a93c20aedd3c8
Umberto M. I. Ferreira <umberto.mif@gmail.com>
2024-01-10 18:21:48 +0900

    Added the lesson controller.

--- course/src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
+++ course/src/main/java/com/ead/course/services/impl/LessonServiceImpl.java
@@ -6,7 +6,9 @@ import com.ead.course.services.LessonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import javax.transaction.Transactional;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service // Spring bean
 public class LessonServiceImpl implements LessonService {
@@ -14,9 +16,23 @@ public class LessonServiceImpl implements LessonService {
     @Autowired
     LessonRepository lessonRepository;
 
-    @Transactional
+    @Override
+    public LessonModel save(LessonModel lessonModel) {
+        return lessonRepository.save(lessonModel);
+    }
+
+    @Override
+    public Optional<LessonModel> findLessonIntoModule(UUID moduleId, UUID lessonId) {
+        return lessonRepository.findLessonIntoModule(moduleId,lessonId);
+    }
+
     @Override
     public void delete(LessonModel lessonModel) {
         lessonRepository.delete(lessonModel);
+    }
+
+    @Override
+    public List<LessonModel> findAllByModule(UUID moduleId) {
+        return lessonRepository.findAllLessonsIntoModule(moduleId);
     }
 }

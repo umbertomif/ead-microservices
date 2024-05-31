d8727246f2cea09c6d0880b65a1d4f75abd66798
Umberto M. I. Ferreira <umberto.mif@gmail.com>
2024-05-31 22:29:51 +0900

    Updated Notification service configurations.

---
+++ notification/src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
@@ -0,0 +1,21 @@
+package com.ead.notification.services.impl;
+
+import com.ead.notification.models.NotificationModel;
+import com.ead.notification.repositories.NotificationRepository;
+import com.ead.notification.services.NotificationService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class NotificationServiceImpl implements NotificationService {
+
+    final NotificationRepository notificationRepository;
+
+    public NotificationServiceImpl(NotificationRepository notificationRepository) {
+        this.notificationRepository = notificationRepository;
+    }
+
+    @Override
+    public NotificationModel saveNotification(NotificationModel notificationModel) {
+        return notificationRepository.save(notificationModel);
+    }
+}

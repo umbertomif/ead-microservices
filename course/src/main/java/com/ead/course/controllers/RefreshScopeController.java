0f62ea0e8778335b3f5001bd4b9617b7c01dc5b7
Umberto M. I. Ferreira <umberto.mif@gmail.com>
2024-05-31 20:56:09 +0900

    Updated authuser and course to use config-server.

---
+++ course/src/main/java/com/ead/course/controllers/RefreshScopeController.java
@@ -0,0 +1,9 @@
+package com.ead.course.controllers;
+
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+public class RefreshScopeController {
+}

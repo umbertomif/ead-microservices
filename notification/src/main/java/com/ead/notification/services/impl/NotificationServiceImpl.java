ae7fb9253d5f06efff5fd5febb00b17c6b0e9599
Umberto M. I. Ferreira <umberto.mif@gmail.com>
2024-06-01 11:07:34 +0900

    Added the controllers endpoints on notification service.

--- notification/src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
+++ notification/src/main/java/com/ead/notification/services/impl/NotificationServiceImpl.java
@@ -3,7 +3,12 @@ package com.ead.notification.services.impl;
 import com.ead.notification.models.NotificationModel;
 import com.ead.notification.repositories.NotificationRepository;
 import com.ead.notification.services.NotificationService;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
+import com.ead.notification.enums.NotificationStatus;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class NotificationServiceImpl implements NotificationService {
@@ -17,5 +22,15 @@ public class NotificationServiceImpl implements NotificationService {
     @Override
     public NotificationModel saveNotification(NotificationModel notificationModel) {
         return notificationRepository.save(notificationModel);
+    }
+
+    @Override
+    public Page<NotificationModel> findAllNotificationsByUser(UUID userId, Pageable pageable) {
+        return notificationRepository.findAllByUserIdAndNotificationStatus(userId, NotificationStatus.CREATED,pageable);
+    }
+
+    @Override
+    public Optional<NotificationModel> findByNotificationIdAndUserId(UUID notificationId, UUID userId) {
+        return notificationRepository.findByNotificationIdAndUserId(notificationId, userId);
     }
 }

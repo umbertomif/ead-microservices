6be3a7adb29ac3de932999a77756755f4435b5ae
Umberto M. I. Ferreira <umberto.mif@gmail.com>
2024-06-18 10:58:51 +0900

    Updated credentials eureka server.

--- service-registry/src/main/java/com/ead/serviceregistry/configs/WebSecurityConfig.java
+++ service-registry/src/main/java/com/ead/serviceregistry/configs/WebSecurityConfig.java
@@ -6,14 +6,12 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
     @Value("${ead.serviceRegistry.username}")
     private String username;
 
